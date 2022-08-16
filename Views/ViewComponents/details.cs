1a74b48bcdc33146dc7adb1a09f7d833f5c3c7ef
BoomBakar <abubakardar608@gmail.com>
2022-08-16 21:12:47 +0500

    View component added

---
+++ Views/ViewComponents/details.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewComponentSample.Models;
+
+namespace WebApplication7.Views.ViewComponents
+{
+    public class detailsViewComponent : ViewComponent
+    {
+        public IViewComponent Invoke()
+        {
+            return View();
+        }
+    }
+}
